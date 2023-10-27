91a4604f57da801340e864a9c81efadd78d4020b
jacques zhu <jacques.zhu19@gmail.com>
2023-10-27 12:45:52 +0200

    first commit

---
+++ a9number_v3 test.py
@@ -0,0 +1,133 @@
+import cProfile
+import re
+
+
+"""In order to run the tests simply run
+
+python -m pytest a9number_v3.py
+
+In order to see the profiling, you need to add the option -s
+"""
+
+import re
+
+def count_occurrences_in_text(word_or_phrase, text):
+    """
+    Return the number of occurrences of the passed word or phrase (case insensitive) in text.
+    If the word_or_phrase contains multiple words, it checks if they occur consecutively.
+    """
+    
+    word_or_phrase = word_or_phrase.lower()
+    text = text.lower()
+        
+    # Modifiez l'expression régulière pour capturer correctement les mots et la ponctuation
+    pattern = r"[\w'-]+|[.,;:!?'\"]"
+
+    # Extraire les éléments de la phrase de recherche
+    word_list = re.findall(pattern, word_or_phrase)
+    
+    # Extraire tous les éléments du texte
+    words = re.findall(pattern, text)
+    print("All elements from text:", words)
+    
+    count = 0
+    for i in range(len(words) - len(word_list) + 1):
+        sequence = words[i:i+len(word_list)]
+        sequence_str = ''.join(sequence)
+        
+        print(f"Comparing sequence '{sequence_str}' with '{word_or_phrase}'")
+        if sequence == word_list:
+            print("Match found! Incrementing count.")
+            count += 1
+
+    print(f"Final count for word or phrase: {count}")
+    return count
+
+    
+def test_count_occurrences_in_text():
+    text = """Georges is my name and I like python. Oh ! your name is georges? And you like Python!
+Yes is is true, I like PYTHON
+and my name is GEORGES"""
+    # test with a little text.
+    assert 3 == count_occurrences_in_text("Georges", text)
+    assert 3 == count_occurrences_in_text("GEORGES", text)
+    assert 3 == count_occurrences_in_text("georges", text)
+    assert 0 == count_occurrences_in_text("george", text)
+    assert 3 == count_occurrences_in_text("python", text)
+    assert 3 == count_occurrences_in_text("PYTHON", text)
+    assert 2 == count_occurrences_in_text("I", text)
+    assert 0 == count_occurrences_in_text("n", text)
+    assert 1 == count_occurrences_in_text("true", text)
+    # regard ' as text:
+    assert 0 == count_occurrences_in_text("maley", "John O'maley is my friend")
+    assert 3 == count_occurrences_in_text("Georges", text)
+    assert 3 == count_occurrences_in_text("GEORGES", text)
+    assert 3 == count_occurrences_in_text("georges", text)
+    assert 0 == count_occurrences_in_text("george", text)
+    assert 3 == count_occurrences_in_text("python", text)
+    assert 3 == count_occurrences_in_text("PYTHON", text)
+    assert 2 == count_occurrences_in_text("I", text)
+    assert 0 == count_occurrences_in_text("n", text)
+    assert 1 == count_occurrences_in_text("true", text)
+    assert 1 == count_occurrences_in_text(
+        "'reflexion mirror'", "I am a senior citizen and I live in the Fun-Plex 'Reflexion Mirror' in Sopchoppy, Florida"
+    )
+    assert 1 == count_occurrences_in_text(
+        "reflexion mirror", "I am a senior citizen and I live in the Fun-Plex (Reflexion Mirror) in Sopchoppy, Florida"
+    )
+    assert 1 == count_occurrences_in_text("reflexion mirror", "Reflexion Mirror\" in Sopchoppy, Florida")
+    assert 1 == count_occurrences_in_text(
+        "reflexion mirror", "I am a senior citizen and I live in the Fun-Plex �Reflexion Mirror� in Sopchoppy, Florida"
+    )
+    assert 1 == count_occurrences_in_text(
+        "reflexion mirror",
+        "I am a senior citizen and I live in the Fun-Plex \u201cReflexion Mirror\u201d in Sopchoppy, Florida"
+    )
+    assert 1 == count_occurrences_in_text(
+        "legitimate", "who is approved by OILS is completely legitimate: their employees are of legal working age"
+    )
+    assert 0 == count_occurrences_in_text(
+        "legitimate their", "who is approved by OILS is completely legitimate: their employees are of legal working age"
+    )
+    assert 1 == count_occurrences_in_text(
+        "get back to me", "I hope you will consider this proposal, and get back to me as soon as possible"
+    )
+    assert 1 == count_occurrences_in_text(
+        "skin-care", "enable Delavigne and its subsidiaries to create a skin-care monopoly"
+    )
+    assert 1 == count_occurrences_in_text(
+        "skin-care monopoly", "enable Delavigne and its subsidiaries to create a skin-care monopoly"
+    )
+    assert 0 == count_occurrences_in_text(
+        "skin-care monopoly in the US", "enable Delavigne and its subsidiaries to create a skin-care monopoly"
+    )
+    assert 1 == count_occurrences_in_text("get back to me", "When you know:get back to me")
+    assert 1 == count_occurrences_in_text(
+        "don't be left", """emergency alarm warning.
+Don't be left unprotected. Order your SSSS3000 today!"""
+    )
+    assert 1 == count_occurrences_in_text(
+        "don", """emergency alarm warning.
+Don't be left unprotected. Order your don SSSS3000 today!"""
+    )
+    assert 1 == count_occurrences_in_text("take that as a 'yes'", "Do I have to take that as a 'yes'?")
+    assert 1 == count_occurrences_in_text("don't take that as a 'yes'", "I don't take that as a 'yes'?")
+    assert 1 == count_occurrences_in_text("take that as a 'yes'", "I don't take that as a 'yes'?")
+    assert 1 == count_occurrences_in_text("don't", "I don't take that as a 'yes'?")
+    assert 1 == count_occurrences_in_text("attaching my c.v. to this e-mail", "I am attaching my c.v. to this e-mail.")
+    assert 1 == count_occurrences_in_text("Linguist", "'''Linguist Specialist Found Dead on Laboratory Floor'''")
+    assert 1 == count_occurrences_in_text(
+        "Linguist Specialist", "'''Linguist Specialist Found Dead on Laboratory Floor'''"
+    )
+    assert 1 == count_occurrences_in_text(
+        "Laboratory Floor", "'''Linguist Specialist Found Dead on Laboratory Floor'''"
+    )
+    assert 1 == count_occurrences_in_text("Floor", "'''Linguist Specialist Found Dead on Laboratory Floor'''")
+    assert 1 == count_occurrences_in_text("Floor", "''Linguist Specialist Found Dead on Laboratory Floor''")
+    assert 1 == count_occurrences_in_text("Floor", "__Linguist Specialist Found Dead on Laboratory Floor__")
+    assert 1 == count_occurrences_in_text("Floor", "'''''Linguist Specialist Found Dead on Laboratory Floor'''''")
+    assert 1 == count_occurrences_in_text("Linguist", "'''Linguist Specialist Found Dead on Laboratory Floor'''")
+    assert 1 == count_occurrences_in_text("Linguist", "''Linguist Specialist Found Dead on Laboratory Floor''")
+    assert 1 == count_occurrences_in_text("Linguist", "__Linguist Specialist Found Dead on Laboratory Floor__")
+    assert 1 == count_occurrences_in_text("Linguist", "'''''Linguist Specialist Found Dead on Laboratory Floor'''''")
+
